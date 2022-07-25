5dd55ac8d21f8bac167f22445fb8cb1943a629bd
G-a-u-r-i <89967629+G-a-u-r-i@users.noreply.github.com>
2022-07-25 20:20:48 +0530

    Create ADMIN1.js

---
+++ ADMIN1.js
@@ -0,0 +1,70 @@
+function func1()
+{
+    var dom1=document.getElementById("one");
+    dom1.style.backgroundColor="black";
+    var dom2=document.getElementById("linkone");
+    dom2.style.color="#FFFF00";
+}
+function func2()
+{
+    var dom1=document.getElementById("two");
+    dom1.style.backgroundColor="black";
+    var dom2=document.getElementById("linktwo");
+    dom2.style.color="#FFFF00";
+}
+function func3()
+{
+    var dom1=document.getElementById("three");
+    dom1.style.backgroundColor="black";
+    var dom2=document.getElementById("linkthree");
+    dom2.style.color="#FFFF00";
+}
+function func4()
+{
+    var dom1=document.getElementById("four");
+    dom1.style.backgroundColor="black";
+    var dom2=document.getElementById("linkfour");
+    dom2.style.color="#FFFF00";
+}
+function func5()
+{
+    var dom1=document.getElementById("five");
+    dom1.style.backgroundColor="black";
+    var dom2=document.getElementById("linkfive");
+    dom2.style.color="#FFFF00";
+}
+function func11()
+{
+    var dom1=document.getElementById("one");
+    dom1.style.backgroundColor="white";
+    var dom2=document.getElementById("linkone");
+    dom2.style.color="black";
+}
+function func22()
+{
+    var dom1=document.getElementById("two");
+    dom1.style.backgroundColor="white";
+    var dom2=document.getElementById("linktwo");
+    dom2.style.color="black";
+}
+function func33()
+{
+    var dom1=document.getElementById("three");
+    dom1.style.backgroundColor="white";
+    var dom2=document.getElementById("linkthree");
+    dom2.style.color="black";
+}
+function func44()
+{
+    var dom1=document.getElementById("four");
+    dom1.style.backgroundColor="white";
+    var dom2=document.getElementById("linkfour");
+    dom2.style.color="black";
+}
+function func55()
+{
+    var dom1=document.getElementById("five");
+    dom1.style.backgroundColor="white";
+    var dom2=document.getElementById("linkfive");
+    dom2.style.color="black";
+}
